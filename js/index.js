2f96e39873c8bb0e604f936d0dfc00ca1eaab3ee
nicolasjbray <35466892+nicolasjbray@users.noreply.github.com>
2018-11-03 20:50:19 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ var _data = JSON.parse(`
    {"line":"Yeah!","time":203000},
    {"line":"Yeah, right!","time":213500},
    {"line":"Pretty good, huh","time":219000},
-   {"line":"Huh!","time":223500},
+   {"line":"Huh!","time":222500},
    {"line":"Yeah, I'm proud to be a part of this number","time":235500},
 
    {"line":"","time":239500},
@@ -83,6 +83,93 @@ var _data = JSON.parse(`
    {"line":"Somehow to defend this place","time":277000},
    {"line":"Everything must be this way","time":281500},
    {"line":"Everything must be this way, yeah","time":286000},
+
+   {"line":"","time":290000},
+
+   {"line":"The Soft Parade has now begun","note":"[Part Five]","time":319000},
+   {"line":"Listen to the engines hum","time":324000},
+   {"line":"People out to have some fun","time":328500},
+   {"line":"A cobra on my left","time":333000},
+   {"line":"Leopard on my right, yeah","time":335500},
+
+    {"line":"","time":339000},
+
+   {"line":"The deer woman in a silk dress","time":342000},
+   {"line":"Girls with beads around their necks","time":347000},
+   {"line":"Kiss the hunter of the green vest","time":352000},
+   {"line":"Who has wrestled before","time":355500},
+   {"line":"With lions in the night","time":359000},
+
+    {"line":"","time":361000},
+
+   {"line":"Out of sight!","time":366000},
+   {"line":"The lights are getting brighter","time":370000},
+   {"line":"The radio is moaning","time":379500},
+   {"line":"Calling to the dogs","time":385000},
+   {"line":"There are still a few animals","time":388000},
+   {"line":"Left out in the yard","time":393500},
+   {"line":"But it's getting harder ","time":397000},
+   {"line":"To describe sailors","time":403000},
+   {"line":"To the underfed","time":411500},
+
+   {"line":"","time":415000},
+
+   {"line":"Tropic corridor","time":422500},
+   {"line":"Tropic treasure","time":425000},
+   {"line":"What got us this far","time":427000},
+   {"line":"To this mild equator?","time":429000},
+
+   {"line":"","time":432000},
+
+   {"line":"We need someone or something new","time":432000},
+   {"line":"Something else to get us through, yeah, c'mon","time":437000},
+
+   {"line":"","time":441000},
+
+   {"line":"Callin' on the dogs","time":445000},
+   {"line":"Callin' on the dogs","time":447000},
+   {"line":"Oh, it's gettin' harder","time":449000},
+   {"line":"Callin' on the dogs","time":800000},
+   {"line":"Callin' in the dogs ","time":800000},
+   {"line":"Callin' all the dogs","time":800000},
+   {"line":"Callin' on the gods","time":800000},
+
+   {"line":"","time":800000},
+
+   {"line":"You gotta meet me","time":800000},
+   {"line":"Too late, baby","time":800000},
+   {"line":"Slay a few animals","time":800000},
+   {"line":"At the crossroads","time":800000},
+   {"line":"Too late","time":800000},
+   {"line":"All in the yard","time":800000},
+   {"line":"But it's gettin' harder","time":800000},
+   {"line":"By the crossroads","time":800000},
+   {"line":"You gotta meet me","time":800000},
+   {"line":"Oh, we're goin', we're goin great","time":800000},
+   {"line":"At the edge of town","time":800000},
+   {"line":"Tropic corridor","time":800000},
+   {"line":"Tropic treasure","time":800000},
+   {"line":"Havin' a good time","time":800000},
+   {"line":"Got to come along","time":800000},
+   {"line":"What got us this far","time":800000},
+   {"line":"To this mild equator?","time":800000},
+   {"line":"Outskirts of the city","time":800000},
+   {"line":"You and I","time":800000},
+   {"line":"We need someone new","time":800000},
+   {"line":"Somethin' new","time":800000},
+   {"line":"Somethin' else to get us through","time":800000},
+   {"line":"Better bring your gun","time":800000},
+   {"line":"Better bring your gun","time":800000},
+   {"line":"Tropic corridor","time":800000},
+   {"line":"Tropic treasure","time":800000},
+   {"line":"We're gonna ride and have some fun","time":800000},
+
+   {"line":"","time":800000},
+
+   {"line":"When all else fails","time":800000},
+   {"line":"We can whip the horse's eyes","time":800000},
+   {"line":"And make them sleep","time":800000},
+   {"line":"And cry","time":800000},
 
    {"line":"","time":800000}]}`);
 
